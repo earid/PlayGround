4caaca7428b52a7a9aaf722f96b2918607acdde0
Earid A <earid777@gmail.com>
2023-10-21 15:25:07 +0600

    Create index.php

---
+++ 9-simple-age-calculetor/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="box">
+        <h1>Age Calculetor</h1>
+        <label for="dob">Enter Date of Birth</label>
+        <input type="date" id="date" placeholder="Enter Date of Birth" />
+        <button id="btn">Find Age</button>
+        <p id="result">Your Age is 55</p>
+      </div>
+    </div>
+  </body>
+  <script src="scripts.js"></script>
+</html>
