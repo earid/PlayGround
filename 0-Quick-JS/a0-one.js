146d39f1c5f7fa5673729c4fa06cc3c5f2f75d1b
Earid A <earid777@gmail.com>
2023-12-14 00:42:11 +0600

    Update a0-one.js

--- 0-Quick-JS/a0-one.js
+++ 0-Quick-JS/a0-one.js
@@ -2,3 +2,10 @@
 function clickMe() {
     window.open('https://www.youtube.com/', '_blank')
 }
+// Get Image Width and Height
+function myFun() {
+    let img = document.getElementById('image');
+    let width = img.naturalWidth;
+    let height = img.naturalHeight;
+    alert('Image Width =' + width + ' Image height = ' + height);
+}
